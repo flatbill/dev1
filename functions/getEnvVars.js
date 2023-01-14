a428324786bfb839a0936a9bd87fc3005db3d889
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-14 17:41:55 -0500

    Update getEnvVars.js

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -1,1 +1,9 @@
-weeeee
+exports.handler = async function (event, context) {
+    const value = process.env.supaUrl
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Value of supaUrl is ${value}.` })
+    }
+  }
+  

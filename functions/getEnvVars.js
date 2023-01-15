1a1373e7377ea077e908c79d70a8eed8890e7966
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-15 10:03:33 -0500

    Update getEnvVars.js

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -1,6 +1,11 @@
-exports.handler =  function () {
-    console.log('running function getEnvVars.js')
-    let supaUrl =  process.env.supaUrl
-    console.log('supaUrl is ' = supaUrl)
-    return supaUrl
+// exports.handler =  function () {
+//     console.log('running function getEnvVars.js')
+//     let supaUrl =  process.env.supaUrl
+//     console.log('supaUrl is ' = supaUrl)
+//     return supaUrl
+// }
+
+exports.handler =  async function(event, context) {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  return context.logStreamName
 }

be9b41604da7eff4730f831f1d5ff1128ea2fa3b
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-15 13:16:32 -0500

    Update getEnvVars.js

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -5,8 +5,11 @@
 //     return supaUrl
 // }
 
+const { request } = require("http")
+
   exports.handler = (event, context) => { 
     console.log('9 running netlify function getEnvVars') 
+    console.log(request.arguments)
     return {
     statusCode: 200,
     headers: {'Access-Control-Allow-Origin': 'localhost:4200'},

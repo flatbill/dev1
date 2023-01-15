eb32f9c6157150f0c319532fabfc1266f53a3255
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-15 11:08:53 -0500

    Update getEnvVars.js

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -7,5 +7,6 @@
 
 exports.handler =  async function(event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  return context.logStreamName
+  return {"firstName":"John", "lastName":"Doe"}
+  //return context.logStreamName
 }

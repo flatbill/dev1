9e63dab65520aa9dbad6098ddb5cf6d10d54f90d
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-15 09:21:31 -0500

    Update getEnvVars.js

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -1,6 +1,6 @@
 exports.handler = async function () {
     console.log('running function getEnvVars.js')
-    // let supaUrl = await process.env.supaUrl
+    let supaUrl = await process.env.supaUrl
     // console.log('supaUrl is ' = supaUrl)
     // return {
     //   statusCode: 200,

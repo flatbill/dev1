e28a71be011254d98cd0cc3c48b63aead2b6758f
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-16 12:48:44 -0500

    Update getEnvVars.js

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -6,15 +6,18 @@
 // }
 
 
-  exports.handler = (event, context) => { 
+  exports.handler = async (event, context) => { 
     console.log('10a running netlify function getEnvVars') 
     console.log( process.env.supaUrl )
     let supaUrl =  process.env.supaUrl 
-
     return {
       statusCode: 200,
-      headers: {'Access-Control-Allow-Origin': '*'},
-      body: supaUrl //JSON.stringify(ret)
+      body: "Hello, Worldy"
     }
+    // return {
+    //   statusCode: 200,
+    //   headers: {'Access-Control-Allow-Origin': '*'},
+    //   body: supaUrl //JSON.stringify(ret)
+    // }
 
 }

ab3e863e6f310cad7ba1393963f618f6d39cc1ab
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-18 09:16:34 -0500

    Update getEnvVars.js

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -1,20 +1,49 @@
-// exports.handler =  function () {
-//     console.log('running function getEnvVars.js')
-//     let supaUrl =  process.env.supaUrl
-//     console.log('supaUrl is ' = supaUrl)
-//     return supaUrl
-// }
 
+let myFaunaCollection = 'qtUsers'
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+const q = faunadb.query
 
-  exports.handler =  (event, context) => { 
-    console.log('10a running netlify function getEnvVars') 
-    console.log( process.env.supaUrl )
-    let supaUrl =  process.env.supaUrl 
+exports.handler = (event, context) => {
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET2
+  }) 
+  const myCust = event.queryStringParameters.cust
+  const myQid = event.queryStringParameters.qid
+
+  console.log('Netlify Function qtReadUsers invoked.')
+  console.log(myCust,myQid)
+  // let myFaunaDbName = 'alaska6'  // not needed cuz FAUNADB_SERVER_SECRET2 is tied to database alaska6.
+  
+  let myFaunaFetchUsers = 'classes/' + myFaunaCollection + '/'
+  //return client.query(q.Paginate(q.Match(q.Ref('indexes/qtUsersX1'))))
+  return client.query(q.Paginate(q.Match(q.Index('qtUsersX1'),[myCust,myQid]),{ size: 500 }))
+    .then((response) => {
+    const qtUsersRefs = response.data
+    console.log('qtUsers refs', qtUsersRefs)
+    console.log(`${qtUsersRefs.length} qtUsers found`)
+    const getAllqtUsersDataQuery = qtUsersRefs.map((ref) => {
+      return q.Get(ref)
+    })
+    // then query the refs
+    return client.query(getAllqtUsersDataQuery).then((ret) => {
+      return {
+        statusCode: 200,
+        headers: {'Access-Control-Allow-Origin': '*'},
+        body: JSON.stringify(ret)
+      }
+    })
+  }).catch((error) => {
+    console.log('error', error)
     return {
-      statusCode: 201,
-      headers: {'Access-Control-Allow-Origin': '*'},
-      body: JSON.stringify({ x: 5, y: 6 })
+      statusCode: 400,
+      body: JSON.stringify(error)
     }
-    
+  })  
+}
+function getIdInline(urlPath) {
+  console.log(`Running getIdInline from Netlify Function 'qtRead03' . urlPath: ${urlPath}`)
 
+  return urlPath.match(/([^\/]*)\/*$/)[0]
 }

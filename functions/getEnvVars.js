ca07152aa0300e4ba06645e666ce4dd42c4d845a
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-20 12:10:27 -0500

    Update getEnvVars.js

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -2,7 +2,7 @@
 let myFaunaCollection = 'qtUsers'
 /* Import faunaDB sdk */
 //const faunadb = require('faunadb')
-const faunadb = require('@supabase') //billy
+const faunadb = require('@supabaseyyy') //billy
 const q = faunadb.query
 
 exports.handler = (event, context) => {

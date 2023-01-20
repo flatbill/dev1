ddb0a4411ff8d9789cb75d7c667b9639097c7200
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-20 12:07:01 -0500

    Add files via upload

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -1,7 +1,8 @@
 
 let myFaunaCollection = 'qtUsers'
 /* Import faunaDB sdk */
-const faunadb = require('faunadb')
+//const faunadb = require('faunadb')
+const faunadb = require('@supabase') //billy
 const q = faunadb.query
 
 exports.handler = (event, context) => {

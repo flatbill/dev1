89894eec56d49f18201664b930a774342b7abc32
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-20 12:52:49 -0500

    Add files via upload

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -1,30 +1,27 @@
 
 let myFaunaCollection = 'qtUsers'
 /* Import faunaDB sdk */
-//const faunadb = require('faunadb')
-const faunadb = require('@supabase') //billy
+const faunadb = require('faunadb')
+const supabase = require('@supabase') //billy
 const q = faunadb.query
+const myQid = '1'
+const myCust = '2'
 
 exports.handler = (event, context) => {
   /* configure faunaDB Client with our secret */
   const client = new faunadb.Client({
     secret: process.env.FAUNADB_SERVER_SECRET2
   }) 
-  const myCust = event.queryStringParameters.cust
-  const myQid = event.queryStringParameters.qid
 
-  console.log('Netlify Function qtReadUsers invoked.')
-  console.log(myCust,myQid)
-  // let myFaunaDbName = 'alaska6'  // not needed cuz FAUNADB_SERVER_SECRET2 is tied to database alaska6.
+  console.log('Netlify Function getEnvVars invoked.')
   
-  let myFaunaFetchUsers = 'classes/' + myFaunaCollection + '/'
-  //return client.query(q.Paginate(q.Match(q.Ref('indexes/qtUsersX1'))))
+  // return client.query(q.Paginate(q.Match(q.Index('qtUsersX1'),[myCust,myQid]),{ size: 500 }))
   return client.query(q.Paginate(q.Match(q.Index('qtUsersX1'),[myCust,myQid]),{ size: 500 }))
     .then((response) => {
-    const qtUsersRefs = response.data
-    console.log('qtUsers refs', qtUsersRefs)
-    console.log(`${qtUsersRefs.length} qtUsers found`)
-    const getAllqtUsersDataQuery = qtUsersRefs.map((ref) => {
+    const responser = response.data
+    console.log('responser from db:', responser)
+    console.log(`${responser.length} responsers found`)
+    const getAllqtUsersDataQuery = responser.map((ref) => {
       return q.Get(ref)
     })
     // then query the refs
@@ -43,8 +40,4 @@ exports.handler = (event, context) => {
     }
   })  
 }
-function getIdInline(urlPath) {
-  console.log(`Running getIdInline from Netlify Function 'qtRead03' . urlPath: ${urlPath}`)
 
-  return urlPath.match(/([^\/]*)\/*$/)[0]
-}

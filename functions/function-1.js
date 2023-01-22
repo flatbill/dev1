2e891f004fe098aa76a6ec6e4f081e6d01a8281d
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-22 13:34:38 -0500

    Update function-1.js

--- functions/function-1.js
+++ functions/function-1.js
@@ -2,6 +2,7 @@ const {helloWorld} = require('./utils')
 
 exports.handler = async (event) => {
   helloWorld()
+  console.log('we ran helloWorld function')
   return {
 		statusCode: 200,
 		body: 'Hello Worldie.',

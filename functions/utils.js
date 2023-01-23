d16b11a9d1c49ef80895905f3de2e8e6f1a6bb0e
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-22 22:00:31 -0500

    Update utils.js

--- functions/utils.js
+++ functions/utils.js
@@ -1,9 +1,9 @@
-exports.helloWorld = () => {
-    console.log('hello world2')
-     console.log('hello world22')
-     console.log('hello world2222')
-     
-}
+//exports.helloWorld = () => {
+//    console.log('hello world2')
+//     console.log('hello world22')
+//     console.log('hello world2222')
+//    
+//}
 exports.anotherFunc = () => {
   console.log('another func')
 }

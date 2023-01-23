0d430eccca5cbbec05e15928f632782c2165df82
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-23 12:38:19 -0500

    Add files via upload

--- functions/readUsers2.js
+++ functions/readUsers2.js
@@ -16,7 +16,7 @@ let myFaunaCollection = 'qtUsers'
 //import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js'
 // const q = faunadb.query //billy
 //let sb = require('@supabase/supabase-js') //billy//
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabasey/supabase-js'
 exports.handler = async (event, context, callback) => {
   console.log('Netlify Function readUsers2 invoked.')
   const myQid = '1'

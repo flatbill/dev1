29be257d7be7ed0a238eef9035477b9be535d32c
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-01-24 12:15:59 -0500

    Add files via upload

---
+++ readUsers2.js
@@ -0,0 +1,24 @@
+//let supabase = require('@supabase/supabase-js') 
+import { createClient } from '@supabase/supabase-js' //same error as require
+
+exports.handler = async (event, context, callback) => {
+  console.log('running Netlify Function readUsers2')
+  const supaUrl = process.env.supaUrl
+  const supaAnonKey = process.env.supaAnonKey
+  console.log('we reached line 6 readUser2.js')
+  console.log(typeof supaUrl )
+  console.log(typeof supaAnonKey )
+  console.log('we reached line 9 readUser2.js')
+
+  const myObj1 = {firstName: "Bill", age: 60, city: "Ocala"}
+  const genericResponse = {
+    statusCode: 200,
+    headers: {'Access-Control-Allow-Origin': '*'},
+    body: JSON.stringify(myObj1)
+  }
+  // console.log(typeof supabase)
+  console.log('we reached line 18 readUser2.js')
+
+  return genericResponse
+}
+  ////////////////////////////////////////

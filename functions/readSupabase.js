d6dc1096c069d134db234b06824892d6056fa3da
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-02-07 17:15:03 -0500

    Add files via upload

--- functions/readSupabase.js
+++ functions/readSupabase.js
@@ -28,14 +28,6 @@ exports.handler = async (event, context) => {
   console.log(maxRows)
   console.log('40 supabaseClient type:')
   console.log(typeof supabaseClient)
-  // const myObj1 = {firstName: "Bill", age: 60, city: "Ocala"}
-  // const genericResponse = {
-  //   statusCode: 200,
-  //   headers: {'Access-Control-Allow-Origin': '*'},
-  //   body: JSON.stringify(myObj1)
-  // }
-  // console.log('myObj1: ', myObj1)
-  console.log('we reached line 48 readSupabase')    
 
   const { data } = await supabaseClient
   .from(tbl).select()
@@ -44,12 +36,10 @@ exports.handler = async (event, context) => {
   .eq(fld3, fldVl3)
   .limit(maxRows)
   console.log('supabase data has type:')
-  // console.log(typeof data)
   // console.table(data)  // only appears when the field name is 'data'. blaming supabase.
   console.log('supabase data: ', data)
   // console.log(data[0].make)  //this works, so data must be an array?
   console.log('we reached line 60 readSupabase')
-
   supabaseData = data
   console.log('supabaseData:')
   console.table(supabaseData)
@@ -69,46 +59,3 @@ exports.handler = async (event, context) => {
   return myResponse  //billy iz this working?
 
 } // end export.handler =>
-  ////////////////////////////////////////
-
-
-// import { createClient } from '@supabase/supabase-js' 
-// const supaUrl = process.env.supaUrl
-// const supaAnonKey = process.env.supaAnonKey
-// const supabaseClient = createClient(supaUrl,supaAnonKey)
-// exports.handler =  (event, context) => {
-//   console.log('running Netlify lambda function: readSupabase')
-//   // const supaUrl = process.env.supaUrl
-//   // const supaAnonKey = process.env.supaAnonKey
-//   // console.log('we reached line 6 readSupabase.js')
-//   // console.log(typeof supaUrl)
-//   // console.log(typeof supaAnonKey)
-//   console.log('we reached line 12 readSupabase.js')
-  
-//   console.log('11 supabaseClient type:')
-//   console.log(typeof supabaseClient)
-//   const myObj1 = {nickName: "Fang", age: 63, city: "Ocala"}
-//   const genericResponse = {
-//     statusCode: 222,
-//     headers: {'Access-Control-Allow-Origin': '*'},
-//     body: JSON.stringify(myObj1)
-//   }
-//   console.log('we reached line 22 readSupabase.js')
-//   console.log(Date.now())
-  
-//   const {data}  = await supabaseClient.from('guitars').select().match({ id: 1 })
-//    supabaseClient.from('guitars').select().match({ id: 1 })
-//   //.then ((response) => {
-    
-//       { statusCode : '206', 
-//         headers: {'Access-Control-Allow-Origin': '*'},
-//         body: JSON.stringify({ data: 'foo'})
-     
-//     // console.log('date now is: ', Date.now())
-//     // console.log(typeof data)
-//     // console.table(data)
-//     // console.log(data)
-//     // } 
-//  // }
-  
-  ////////////////////////////////////////

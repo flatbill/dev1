fba00190204cbd5432ec83f42a29807ca46ec043
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-02-12 16:45:31 -0500

    Add files via upload

---
+++ functions/appendSupabase.js
@@ -0,0 +1,46 @@
+import { createClient } from '@supabase/supabase-js' 
+const supaUrl = process.env.supaUrl
+const supaAnonKey = process.env.supaAnonKey
+const supabaseClient = createClient(supaUrl,supaAnonKey)
+//  
+exports.handler = async (event, context) => {
+  console.log('7 running Netlify lambda function: appendSupabase')
+  const qsParms = event.queryStringParameters
+  // console.table(qsParms)
+  let tbl  = qsParms.tbl            || 'guitarsy'
+  let tblObj = qsParms.tblObj       || 'makey'
+
+  let maxRows = qsParms.maxRows || '1'
+  console.log(' tbl: ',tbl)
+  console.log(' tblObj: ',tblObj)
+  
+  const { data , error } = await supabase
+.from('qtAnswers')    //(tbl)
+.insert([
+  {'cust': '2', 'qid': '1'}
+])
+
+if (error){console.log('error from appendSupabase.',error)}
+if (data){console.log('got data from appendSupabase.',data)}
+
+
+  // const { data } = await supabaseClient
+  // .from(tbl).select()
+  // .eq(fld1, fldVl1)
+  // .eq(fld2, fldVl2)
+  // .eq(fld3, fldVl3)
+  // .eq(fld4, fldVl4)
+  // .limit(maxRows)
+  console.log('we reached line 32 readSupabase')
+  supabaseData = data //supabase seems to like the word 'data'
+  console.log('supabaseData:')
+  console.table(supabaseData)
+  let myObj2 = {  supabaseData }
+  const myResponse = {
+    statusCode: 201,
+    headers: {'Access-Control-Allow-Origin': '*'},
+    body:  JSON.stringify(myObj2)
+  }
+  console.log('we reached the end of appendSupabase. ready to return.')
+  return myResponse   
+} // end export.handler 

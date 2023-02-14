6a1534812b7a14eb8762fb97d7f5a52e18238c11
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-02-14 11:23:14 -0500

    Add files via upload

--- functions/readSupabase.js
+++ functions/readSupabase.js
@@ -7,16 +7,16 @@ exports.handler = async (event, context) => {
   console.log('7 running Netlify lambda function: readSupabase')
   const qsParms = event.queryStringParameters
   // console.table(qsParms)
-  let tbl  = qsParms.tbl        || 'guitarsy'
-  let fld1 = qsParms.fld1       || 'makey'
-  let fld1v = qsParms.fld1v   || 'Washburny'
-  let fld2 = qsParms.fld2       || 'makey'
-  let fld2v = qsParms.fld2v   || 'Washburny'
-  let fld3 = qsParms.fld3       || 'makey'
-  let fld3v = qsParms.fld3v   || 'Washburny'
-  let fld4 = qsParms.fld4       || 'makey'
-  let fld4v = qsParms.fld4v   || 'Washburny'
-  let maxRows = qsParms.maxRows || '1'
+  let tbl  = qsParms.tbl         || 'guitarsy'
+  let fld1 = qsParms.fld1        || 'makey'
+  let fld1v = qsParms.fld1v      || 'Washburny'
+  let fld2 = qsParms.fld2        || fld1
+  let fld2v = qsParms.fld2v      || fld1v
+  let fld3 = qsParms.fld3        || fld1
+  let fld3v = qsParms.fld3v      || fld1v
+  let fld4 = qsParms.fld4        || fld1
+  let fld4v = qsParms.fld4v      || fld1v
+  let maxRows = qsParms.maxRows   || '1'
   console.log(' tbl: ',tbl)
   console.log(' fld1: ',fld1)
   console.log(' fld1v: ',fld1v)

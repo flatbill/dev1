eceafadba7728a0c6f8e0610a78035afd210e536
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-02-18 09:26:24 -0500

    Add files via upload

--- functions/appendSupabase.js
+++ functions/appendSupabase.js
@@ -7,33 +7,33 @@ exports.handler = async (event, context) => {
   console.log('7 running Netlify lambda function: appendSupabase')
   const qsParms = event.queryStringParameters
   let tbl  = qsParms.tbl         || 'defaultTable'
+  console.log('10 appendSupabase qsParms and tbl:')
   console.table(qsParms)
   console.table(tbl)
-  console.log('12 lambda function appendSupabase')
-  let pissTxt = '{'
+  console.log('13  appendSupabase')
+  let myTxt = '{'
   let dq = '"'
   for (let [myKey, myValue] of Object.entries(qsParms)) {
     if(myKey!='tbl'){//we only want field names, not the table name.
-      pissTxt += dq + myKey + dq + ':' + dq + myValue + dq + ','
+      myTxt += dq + myKey + dq + ':' + dq + myValue + dq + ','
     }
  }
- pissTxt = pissTxt.substring(0, pissTxt.length - 1) + '}' // remove last comma, stick bracket on end.
- console.log(pissTxt)
+ myTxt = myTxt.substring(0, myTxt.length - 1) + '}' // remove last comma, stick bracket on end.
+ console.log('22 appendSupabase myTxt:')
+ console.log(myTxt)
 
   console.log('52 lambda function appendSupabase')
-  console.log('66 lambda function appendSupabase')
-  let fldsObj= JSON.parse(pissTxt)
-  console.log(fldsObj)
-  console.log('74 ready to run supabaseClient')
+  let fldsObj= JSON.parse(myTxt)
+  console.log('27 appendSupabase fldsObj:',fldsObj)
   const { data , error } = await supabaseClient
   .from(tbl) //("qtAnswers")    //(tbl)  //tblObj {'cust': '62', 'qid': '116'}
   .insert( fldsObj )
- .select()
+  .select()
 
 if (error){console.log('error from appendSupabase.',error)}
 if (data){console.log('got data from appendSupabase.',data)}
 
-  console.log('we reached line 30 appendSupabase')
+  console.log('we reached line 36 appendSupabase')
   supabaseData = data //supabase seems to like the word 'data'
   console.log('supabaseData:')
   console.table(supabaseData)

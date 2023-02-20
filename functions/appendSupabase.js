87db8b68395ac899fa3f5c2ef299fcb1b77c0716
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-02-20 08:39:35 -0500

    Add files via upload

--- functions/appendSupabase.js
+++ functions/appendSupabase.js
@@ -19,6 +19,8 @@ exports.handler = async (event, context) => {
     } // end if
   }  // end for
   myTxt = myTxt.substring(0, myTxt.length - 1) + '}' // remove last comma, stick bracket on end.
+  myTxt = (myTxt.replaceAll('[', '{'))
+  myTxt = (myTxt.replaceAll(']', '}'))
   console.log('22 appendSupabase myTxt:')
   console.log(myTxt)
   let myFldsObj= JSON.parse(myTxt)

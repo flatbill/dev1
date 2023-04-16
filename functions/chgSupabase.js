84fcdd508fdf704337b7999eb86ab109ef1580f0
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-04-15 21:45:09 -0400

    Add files via upload

--- functions/chgSupabase.js
+++ functions/chgSupabase.js
@@ -7,67 +7,72 @@ exports.handler = async (event, context) => {
   console.log('7 running Netlify lambda function: chgSupabase')
   let qsParms = event.queryStringParameters
   let tbl  = qsParms.tbl         || 'defaultTable'
-  let fld1 = ''
-  let fld1v = ''
-  let fld2 = ''
-  let fld2v = ''
-  let fld3 = ''
-  let fld3v = ''
-  let fld4 = ''
-  let fld4v = ''
+  // let fld1 = ''
+  // let fld1v = ''
+  // let fld2 = ''
+  // let fld2v = ''
+  // let fld3 = ''
+  // let fld3v = ''
+  // let fld4 = ''
+  // let fld4v = ''
   console.log('18 chgSupabase qsParms and tbl:')
   console.table(qsParms)
   console.table(tbl)
   console.log('21  chgSupabase')
   let myTxt = '{'
   let dq = '"'
-  let ii = 1
+  let id = 0
+  // let ii = 1
   for (let [myKey, myValue] of Object.entries(qsParms)) {
     if(myKey!='tbl'){//we only want field names, not the table name.
       myTxt += dq + myKey + dq + ':' + dq + myValue + dq + ','
-      if (ii==1){
-        fld1= myKey
-        fld1v=myValue
-      }
-      if (ii==2){
-        fld2= myKey
-        fld2v=myValue
-      }
-      if (ii==3){
-        fld3= myKey
-        fld3v=myValue
-      }
-      if (ii==4){
-        fld4= myKey
-        fld4v=myValue
-      }
-      ii=ii+1 // loop increment, set one of these: fld1,2,3,4
-    } // end if
+      if (myKey == 'id'){id = myValue}
+      // if (ii==1){
+      //   fld1= myKey
+      //   fld1v=myValue
+      // }
+      // if (ii==2){
+      //   fld2= myKey
+      //   fld2v=myValue
+      // }
+      // if (ii==3){
+      //   fld3= myKey
+      //   fld3v=myValue
+      // }
+      // if (ii==4){
+      //   fld4= myKey
+      //   fld4v=myValue
+      // }
+      // ii=ii+1 // loop increment, set one of these: fld1,2,3,4
+    } // end outer if
   }  // end for
 
   //hack. can have up to to four supabase keys, but not all tables have four keys.
   //  so, re-use field1 as field2,3,4.
-  if (fld2 == ''){ // there aint no field2, so set field2 same as field1.
-    fld2=fld1
-    fld2v=fld1v
-  }
-  if (fld3 == ''){ // there aint no field3, so set field3 same as field1.
-    fld3=fld1
-    fld3v=fld1v
-  }
-  if (fld4 == ''){// there aint no field4, so set field4 same as field1.
-    fld4=fld1
-    fld4v=fld1v
-  }
-  console.log('62 ',fld1,fld1v,fld2,fld2v,fld3,fld3v,fld4,fld4v)
+  // if (fld2 == ''){ // there aint no field2, so set field2 same as field1.
+  //   fld2=fld1
+  //   fld2v=fld1v
+  // }
+  // if (fld3 == ''){ // there aint no field3, so set field3 same as field1.
+  //   fld3=fld1
+  //   fld3v=fld1v
+  // }
+  // if (fld4 == ''){// there aint no field4, so set field4 same as field1.
+  //   fld4=fld1
+  //   fld4v=fld1v
+  // }
+  console.log('64 id: ',id)
   // make sure caller is passing in 'id' as fld1
-  if (fld1 != 'id') {return}
-  
+  if (id == 0) {
+    console.log('caller made an error. he should pass in "id".')
+    return
+  }
+
   console.log('63 gonna await supabaseClient')
   let { data } = await supabaseClient
   .from(tbl)
   .update(myTxt)
-  .eq(fld1, fld1v)
+  .eq('id', id)
   // .eq(fld2, fld2v)
   // .eq(fld3, fld3v)
   // .eq(fld4, fld4v)

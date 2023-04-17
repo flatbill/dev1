60d4953828c3ff53c3a0de3ee60488500a78a29b
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-04-17 06:47:10 -0400

    Add files via upload

--- functions/chgSupabase.js
+++ functions/chgSupabase.js
@@ -2,48 +2,46 @@ import { createClient } from '@supabase/supabase-js'
 const supaUrl = process.env.supaUrl
 const supaAnonKey = process.env.supaAnonKey
 const supabaseClient = createClient(supaUrl,supaAnonKey)
-//  
 exports.handler = async (event, context) => {
-  console.log('7 running Netlify lambda function: chgSupabase')
+  console.log('6 running Netlify lambda function: chgSupabase')
   let qsParms = event.queryStringParameters
   let tbl  = qsParms.tbl         || 'defaultTable'
   let id   = qsParms.id          || '0'
-
-  console.log('14 chgSupabase qsParms and tbl:')
+  console.log('10 chgSupabase qsParms and tbl:')
   console.table(qsParms)
   console.table(tbl)
+  console.log('13 id: ', id)
 
   if (id == '0') {
-    console.log('19 caller made an error. he should pass in "id".')
+    console.log('16 caller made an error. he should pass in "id".')
     return
   }
 
-  console.log('23  chgSupabase')
   let myTxt = '{'
   let dq = '"'
   for (let [myKey, myValue] of Object.entries(qsParms)) {
-    if(myKey!='tbl' && myKey!='id'){
-      //we only want field names, not the table name or rec id.
+    if(myKey!='tbl' && myKey!='id'){ //we only want field names, not the table name or rec id.
       myTxt += dq + myKey + dq + ':' + dq + myValue + dq + ','
-    } // end outer if
+    } // end if
   }  // end for
-  //replace last char with '}'
-  myTxt = myTxt.substring(0, myTxt.length - 1) + '}'
-  console.log('32 myTxt:',myTxt)
-  console.log('33 id: ', id)
+  myTxt = myTxt.substring(0, myTxt.length - 1) + '}' //replace last char with '}'
+  console.log('28 myTxt:',myTxt)
 
   let myObj = new Object
   myObj = JSON.parse(myTxt)
-  console.log('37 gonna await supabaseClient')
+  console.log('32 myObj:')
+  console.table(myObj)
+  console.log('33 gonna await supabaseClient')
+
   let { data } = await supabaseClient
   .from(tbl)
   .update(myObj)
   .eq('id', id)
   .select()
 
-  console.log('we reached line 44 chgSupabase (after supabase call')
+  console.log('we reached line 42 chgSupabase (after supabase call')
   supabaseData = data //supabase seems to like the word 'data'
-  console.log('46 supabaseData:')
+  console.log('44 supabaseData:')
   console.table(supabaseData)
   let myObj2 = {  supabaseData }
   let myResponse = {
@@ -51,6 +49,6 @@ exports.handler = async (event, context) => {
     headers: {'Access-Control-Allow-Origin': '*'},
     body:  JSON.stringify(myObj2)
   }
-  console.log('we reached the end of chgSupabase. ready to return.')
+  console.log('52 we reached the end of chgSupabase. ready to return.')
   return myResponse   
 } // end export.handler 
--- functions/delSupabase.js
+++ functions/delSupabase.js
@@ -5,43 +5,30 @@ const supabaseClient = createClient(supaUrl,supaAnonKey)
 exports.handler = async (event, context) => {
   console.log('7 running Netlify lambda function: delSupabase')
   let qsParms = event.queryStringParameters
-  let tbl  = qsParms.tbl         || 'defaultTable'
-  let id   = qsParms.id           || 'defauldId'
+  let tbl  = qsParms.tbl          || 'defaultTable'
+  let id   = qsParms.id           || '0'
   console.log('10 delSupabase qsParms and tbl:')
   console.table(qsParms)
   console.table(tbl)
-  console.log('13  delSupabase')
-  let myTxt = '{'
-  let dq = '"'
-  for (let [myKey, myValue] of Object.entries(qsParms)) {
-    if(myKey!='tbl'){//we only want field names, not the table name.
-      myTxt += dq + myKey + dq + ':' + dq + myValue + dq + ','
-    } // end if
-  }  // end for
+  console.log('13 id: ', id)
 
-  for (let [myKey, myValue] of Object.entries(qsParms)) {
-    if(myKey!='tbl'&&myKey!='id'){//we only want field names, not table name, not id
-      myTxt += dq + myKey + dq + ':' + dq + myValue + dq + ','
-    } // end if
-  }  // end for
-  console.log('27 gonna await supabaseClient')
+  if (id == '0') {
+    console.log('16 caller made an error. he should pass in "id".')
+    return
+  }
 
-  myTxt = myTxt.substring(0, myTxt.length - 1) + '}' // remove last comma, stick bracket on end.
-  console.log('30 delSupabase myTxt:')
-  console.log(myTxt)
-  let myFldsObj= JSON.parse(myTxt)
-  console.log('33 delSupabase myFldsObj:',myFldsObj)
-  console.log('34 delSupabase id:',id)
+  console.log('20 gonna await supabaseClient')
+
   let { data , error } = await supabaseClient
   .from(tbl)              // ("qtAnswers")    
   .delete()
   .eq('id',id)
   .select()
- if (error){console.log('error from delSupabase.',error)}
- if (data){console.log('got data from delSupabase.',data)}
-  console.log('we reached line 43 delSupabase')
+ if (error){console.log('27 error from delSupabase.',error)}
+ if (data){console.log('28 got data from delSupabase.',data)}
+  console.log('we reached line 29 delSupabase')
   let supabaseData = data //supabase seems to like the word 'data'
-  console.log('supabaseData:')
+  console.log('31 supabaseData:')
   console.table(supabaseData)
   let myObj2 = {  supabaseData }
   let myResponse = {
@@ -49,6 +36,6 @@ exports.handler = async (event, context) => {
     headers: {'Access-Control-Allow-Origin': '*'},
     body:  JSON.stringify(myObj2)
   }
-  console.log('52 we reached the end of delSupabase lambda. ready to return.')
+  console.log('39 we reached the end of delSupabase lambda. ready to return.')
   return myResponse   
 } // end export.handler 

71ba8aa3ba26ad08d9a03eebeb6ff4167d38bcea
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-05-31 09:36:16 -0400

    Add files via upload

--- functions/chgSupabase.js
+++ functions/chgSupabase.js
@@ -21,6 +21,7 @@ exports.handler = async (event, context) => {
   let dq = '"'
   for (let [myKey, myValue] of Object.entries(qsParms)) {
     if(myKey!='tbl' && myKey!='id'){ //we only want field names, not the table name or rec id.
+      // myValue = myValue.replaceAll(',', dq + myValue + dq)
       myTxt += dq + myKey + dq + ':' + dq + myValue + dq + ','
       myTxt  =  myTxt.replaceAll('"['  , '["') //fix array fields
       myTxt  =  myTxt.replaceAll(']"'  , '"]') //fix array fields

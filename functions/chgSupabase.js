5606105256e717754d6d02eed4da262d7a9d7cf9
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-06-04 11:10:27 -0400

    Add files via upload

--- functions/chgSupabase.js
+++ functions/chgSupabase.js
@@ -22,10 +22,11 @@ exports.handler = async (event, context) => {
   for (let [myKey, myValue] of Object.entries(qsParms)) {
     if(myKey!='tbl' && myKey!='id'){ //we only want field names, not the table name or rec id.
       // myValue = myValue.replaceAll(',', dq + myValue + dq)
+      console.log('mValue:)',myValue)
       if( myValue.includes(',') ) {console.log ('25 myValue contains comma')}
       myTxt += dq + myKey + dq + ':' + dq + myValue + dq + ','
-      myTxt  =  myTxt.replaceAll('"['  , '["') //fix array fields
-      myTxt  =  myTxt.replaceAll(']"'  , '"]') //fix array fields
+      // myTxt  =  myTxt.replaceAll('"['  , '["') //fix array fields
+      // myTxt  =  myTxt.replaceAll(']"'  , '"]') //fix array fields
 
     } // end if
   }  // end for
